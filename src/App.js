cea3a3405c92c8872f7f3a68aa74922472933e0d
L0nelyleob <l0nelyleob@gmail.com>
2022-10-21 18:16:37 +0300

    create form with static data and planned to add axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Form from "./components/form/Form";
 import stat from "./static.json"
-
+import axios from "axios";
 
 function App() {
+  let json = JSON.stringify({"name": "usrr2", "password": "felani"})
+  let json2 = JSON.stringify({
+    "name":"usrr6",
+    "desc": "felani55",
+    "anon": true
+})
+
+  let req = axios.post("http://localhost:7000/forms/create", json2, {withCredentials: true}).then(res => console.log(res))
+
   return (
     <div className="App">
       <Form json={stat}/>
