090eb6775e6cbe31a416b67ee2f681efa5df6489
hosur_ <skyebr@ynadex.ru>
2022-10-30 15:51:54 +0300

    pomogite s zaprosami

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,7 @@
 import Header from "../header/header";
-import axios from "axios";
+
 
 function App() {
-    let json = JSON.stringify({"name": "name", "password": "password"})
-    axios.post("http://localhost:7000/auth/token", json, {withCredentials: true}).then(data => console.log(data))
 
   return (
       <div className="App">
--- src/components/signIn/signIn.jsx
+++ src/components/signIn/signIn.jsx
@@ -1,9 +1,40 @@
 import "./signInStyle.css"
+import {useState} from "react";
+import axios from "axios";
+// import authUsr from "./authUsr";
+
 
 function SignIn(){
+    const[name,setName] = useState('')
+    const[password,setPassword] = useState('')
+    const url = 'http://localhost:7000/auth/token'
+
+                                            //          AXIOS FUNC
+    async function authUsr(name,password,url){
+        const data = {
+                       name: name,
+                       password: password,
+                   }
+        await axios.post(url, JSON.stringify(data), {withCredentials: true})
+    }
+
+
+                                                //          FETCH FUNC
+ // async function authUsr(name,password,url){
+ //        const data = {
+ //            name: name,
+ //            password: password,
+ //        }
+ //        let response = await fetch(url,{
+ //            method:'POST',
+ //            body: JSON.stringify(data),
+ //        })
+ //    }
+
+
     return(
         <div>
-            <form method="POST" className="signForm">
+            <form method="POST"  className="signForm">
 
                 <h1 className="formTitle"> <b>Sign-In Form</b> </h1>
 
@@ -15,15 +46,16 @@ function SignIn(){
                 </div>
 
                 <label htmlFor="" className="">Name:</label> <br/>
-                <input name="name" type="text" required className=""/> <br/>
+                <input value={name} onChange={event => setName(event.target.value)} name="name" type="text" required className=""/> <br/>
 
                 <label htmlFor="" className="">Password:</label><br/>
-                <input  name="password" type="password" required className=""/><br/>
+                <input value={password} onChange={event => setPassword(event.target.value)} name="password" type="password" required className=""/><br/>
 
-
-                <button className="" type="submit">Send</button>
+                <button className="" onClick={ () => authUsr(name, password, url) } type="submit">Send</button>
 
             </form>
+
+
         </div>
     )
 }
