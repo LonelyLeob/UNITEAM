045d2429c646af3076a655ced38808f6de209a6e
lab <skyebr@yandex.ru>
2022-11-07 12:24:43 +0300

    added mapping forms

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,26 +2,20 @@ import Field from "../field/Field";
 
 
 function Form(props) {
+    if (props.item.Fields == null) {
+        return (
+            <div key={props.key} className=" mx-40 inline-block p-5 border-double border-4 border-cyan-400 max-w-3xl bg-slate-100">
+                    <h1 className="text-lg font-semibold pb-5">{props.item.Name}</h1>
+                    <p className="pb-5">{props.item.Description}</p>
+            </div>
+        );
+    }
 
     return (
-        <div>
-                {props.getJson.map((item, idx) => {
-                    if (item.Fields <= 1){
-                        return(
-                            <div key={idx} className="mx-40 inline-block p-5 border-double border-4 border-cyan-400 max-w-3xl bg-slate-100">
-                                <h1 className="text-lg font-semibold pb-5">{item.Name}</h1>
-                                <p className="pb-5">{item.Description}</p>
-                            </div>
-                    )
-                    }
-                    return (
-                    <div key={idx} className=" mx-40 inline-block p-5 border-double border-4 border-cyan-400 max-w-3xl bg-slate-100">
-                        <h1 className="text-lg font-semibold pb-5">{item.Name}</h1>
-                        <p className="pb-5">{item.Description}</p>
-                        <Field fields={item.Fields}/>
-                    </div>
-                    )
-                })}
+        <div className=" mx-40 inline-block p-5 border-double border-4 border-cyan-400 max-w-3xl bg-slate-100">
+                <h1 className="text-lg font-semibold pb-5">{props.item.Name}</h1>
+                <p className="pb-5">{props.item.Description}</p>
+            <Field fields={props.item.Fields}/>
         </div>
     );
 }
--- src/components/form/allForms.jsx
+++ src/components/form/allForms.jsx
@@ -1,33 +1,28 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
+// import {useEffect, useState} from "react";
+// import axios from "axios";
 import Form from "./Form"
+import data from "../../jj.json"
 
 
 function AllForms(){
 
-    const [data, setData] = useState([])
+    // const [data, setData] = useState([])
 
 
-    useEffect(() => {
-        axios
-            .get("http://localhost:8080/get/forms", {withCredentials: true})
-            .then(data => {
-                setData(data.data)
-                console.log(data)
-            }).catch(err => {
-            console.log(err)
-        })
-    }, [])
+    // useEffect(() => {
+    //     axios
+    //         .get("http://localhost:8080/get/forms", {withCredentials: true})
+    //         .then(data => {
+    //             setData(data.data)
+    //             console.log(data)
+    //         }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }, [])
 
-    return(
+    return (
         <div>
-            {data.map((item, idx) => {
-                return(
-                    <div key={idx}>
-                        <Form getJson = {item}/>
-                    </div>
-                )
-            })}
+            {data.map((item, idx) => {return (<Form key={idx} item={item}/>)})}
         </div>
     )
 }
